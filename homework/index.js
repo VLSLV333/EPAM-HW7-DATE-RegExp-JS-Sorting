0e9c1ca1d2fc2a12278e3cf48e54514f225f1bdd
VLSLV333 <v.sherstyk@gmail.com>
2022-10-11 17:38:21 +0000

    to slow!

--- homework/index.js
+++ homework/index.js
@@ -16,9 +16,6 @@ Date.prototype.subtractHours = function(h) {
 
 // console.log(today.getTimezoneOffset())
 
-// let birthday22 = new Date(2022, 9, 14, 23, 55, 59);
-
-// console.log(birthday22);
 
 
 
@@ -29,7 +26,43 @@ function getWeekDay(date){
     let options = {weekday: 'long'};
     return date.toLocaleString('en-Uk',options);
 }
+function getAmountDaysToNewYear(){
+    return;
+}
 
+// let today = new Date();
+let today = new Date(2022, 9, 11, 0, 0, 0);
+// let birthday22 = new Date(2022, 0, 1, 0, 0, 0);
+
+console.log(today);
+
+let nextYear = new Date(today.getFullYear() + 1 + "");
+let nextYear1 = new Date("2023").subtractHours(2);
+
+console.log(nextYear);
+console.log(nextYear1);
+
+// instead of dividing seconds we should add them to 1970 to see how many days into they will convert, after that just get days from it!
+
+let milisecondsToNewYear = nextYear1.getTime() - today.getTime();
+let daysToNewYear = Math.round(milisecondsToNewYear/ 1000 / 60 / 60 / 24)
+
+console.log(daysToNewYear);
+
+// console.log(new Date(nextYear1));
+
+
+
+// console.log(birthday22);
+// console.log(birthday22.getFullYear());
+
+// console.log(birthday22.toUTCString());
+// console.log(birthday22.getUTCFullYear());
+
+
+// let birthday22 = new Date(2022, 9, 14, 23, 55, 59);
+
+// console.log(birthday22);
 
 
 // let today = new Date();
@@ -41,4 +74,4 @@ function getWeekDay(date){
 // var d = new Date();
 // var utcDate = new Date(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate(), d.getUTCHours(), d.getUTCMinutes(), d.getUTCSeconds(), d.getUTCMilliseconds());
 
-// console.log(d.getUTCFullYear())
+// console.log(d.getUTCMilliseconds())
