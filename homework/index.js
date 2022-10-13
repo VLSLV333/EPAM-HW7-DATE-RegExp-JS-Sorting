9387b8d9f412f6b78914adcfcd95f035f0a22c20
VLSLV333 <v.sherstyk@gmail.com>
2022-10-13 09:40:35 +0000

    great news! 50% of the task is done, small break and up to func #7

--- homework/index.js
+++ homework/index.js
@@ -73,75 +73,17 @@ function howFarIs(date){
     }
 }
 
+function matchExact(r, str) {
+    let match = str.match(r);
+    return match && str === match[0];
+}
+ function isValidIdentifier(string){
+    if (string === undefined){
+        return false
+    } else {
+        let myRegExp = new RegExp ('^[a-z\$\_]{1,}[a-z0-9\$\_]{0,}$', "i")
+        return myRegExp.test(string);
+    }
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let today = new Date();
-// let today = new Date(2022, 9, 11, 0, 0, 0);
-// // let birthday22 = new Date(2022, 0, 1, 0, 0, 0);
-
-// console.log(today);
-
-// let nextYear = new Date(today.getFullYear() + 1 + "");
-// let nextYear1 = new Date("2023").subtractHours(2);
-
-// console.log(nextYear);
-// console.log(nextYear1);
-
-// // instead of dividing seconds we should add them to 1970 to see how many days into they will convert, after that just get days from it!
-
-// let milisecondsToNewYear = nextYear1.getTime() - today.getTime();
-// let daysToNewYear = Math.round(milisecondsToNewYear/ 1000 / 60 / 60 / 24)
-
-// console.log(daysToNewYear);
-
-// console.log(new Date(nextYear1));
-
-
-
-// console.log(birthday22);
-// console.log(birthday22.getFullYear());
-
-// console.log(birthday22.toUTCString());
-// console.log(birthday22.getUTCFullYear());
-
-
-// let birthday22 = new Date(2022, 9, 14, 23, 55, 59);
-
-// console.log(birthday22);
-
-
-// let today = new Date();
-
-// console.log(birthday22);
-// console.log(birthday22.toLocaleString('en-Uk',options));
-// console.log(birthday22.toUTCString());
-
-// var d = new Date();
-// var utcDate = new Date(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate(), d.getUTCHours(), d.getUTCMinutes(), d.getUTCSeconds(), d.getUTCMilliseconds());
-
-// console.log(d.getUTCMilliseconds())
