ce3f0df1b8624ef054244f21ea788524c3817626
VLSLV333 <v.sherstyk@gmail.com>
2022-10-14 10:33:04 +0000

    rework regexp

--- homework/index.js
+++ homework/index.js
@@ -1,5 +1,6 @@
 function getAge(date){
-    return new Date(new Date().getTime() - date.getTime()).getUTCFullYear() - 1970;
+    let year0 = 1970;
+    return new Date(new Date().getTime() - date.getTime()).getUTCFullYear() - year0;
 }
 function getWeekDay(date){
     let options = {weekday: 'long'};
@@ -8,15 +9,17 @@ function getWeekDay(date){
 function getAmountDaysToNewYear(){
     let today=new Date();
     let newYear=new Date(today.getFullYear()+1, 0, 1);
-    let one_day=1000*60*60*24;
-    let splitedDaysToNewYear = ((newYear.getTime()-today.getTime())/(one_day) + "").split(".");
+    let milS = 1000; let sec = 60; let min = 60; let hour = 24;
+    let one_day=milS*sec*min*hour;
+    let splitedDaysToNewYear = ((newYear.getTime()-today.getTime())/one_day + '').split('.');
     if (splitedDaysToNewYear.length === 1){
-        return Math.round((newYear.getTime()-today.getTime())/(one_day))
+        return Math.round((newYear.getTime()-today.getTime())/one_day)
     } else {
-        if (+splitedDaysToNewYear[1][0] === 0 && +splitedDaysToNewYear[1][1] <= 4){
-            return Math.floor((newYear.getTime()-today.getTime())/(one_day))
+        let secondNumberAfterDot = 4;
+        if (+splitedDaysToNewYear[1][0] === 0 && +splitedDaysToNewYear[1][1] <= secondNumberAfterDot){
+            return Math.floor((newYear.getTime()-today.getTime())/one_day)
         } else {
-            return Math.ceil((newYear.getTime()-today.getTime())/(one_day))
+            return Math.ceil((newYear.getTime()-today.getTime())/one_day)
         }
     }
 }
@@ -24,7 +27,8 @@ function getProgrammersDay(date){
     if (date === undefined){
         return 'Please, provide year'
     } else {
-        if (date%4 === 0){
+        let dividerForLeapYear = 4;
+        if (date%dividerForLeapYear === 0){
             return `12 Sep, ${date} (${getWeekDay(new Date(date, 8, 12))})`
         } else {
             return `13 Sep, ${date} (${getWeekDay(new Date(date, 8, 13 ))})`
@@ -35,13 +39,13 @@ function howFarIs(date){
     try{
         let today = new Date();
         let weekDayValues = {
-            "Monday" : 1,
-            "Tuesday" : 2,
-            "Wednesday" : 3,
-            "Thursday" : 4,
-            "Friday" : 5,
-            "Saturday" : 6,
-            "Sunday" : 7,
+            'Monday' : 1,
+            'Tuesday' : 2,
+            'Wednesday' : 3,
+            'Thursday' : 4,
+            'Friday' : 5,
+            'Saturday' : 6,
+            'Sunday' : 7
         }
         let todaysValue = weekDayValues[getWeekDay(today)];
         let input = date[0].toUpperCase() + date.slice(1);
@@ -51,7 +55,8 @@ function howFarIs(date){
         } else if (todaysValue < inputValue){
             return `It's ${inputValue-todaysValue} day(s) left till ${input}.`
         } else if (todaysValue > inputValue){
-            return `It's ${7-todaysValue+inputValue} day(s) left till ${input}.`
+            let weekValue = 7;
+            return `It's ${weekValue-todaysValue+inputValue} day(s) left till ${input}.`
         } else if (todaysValue === inputValue){
             return `Hey, today is ${input} =)`
         }
@@ -63,17 +68,18 @@ function isValidIdentifier(string){
     if (string === undefined){
         return 'Please, provide your variable'
     } else {
-        let myRegExp = new RegExp ('^[a-z\$\_]{1,}[a-z0-9\$\_]{0,}$', "i")
+        let myRegExp = new RegExp('^[a-z$_]{1,}[a-z0-9$_]{0,}$', 'i')
         return myRegExp.test(string);
     }
 }
+function upperCaser(x){
+    return x.toUpperCase();
+}
 function capitalize(str){
     try{
-        function upperCaser(x){
-            return x.toUpperCase();
-        }
-        let myRegExp = new RegExp ('(^|\\s)([a-z])', "ig")
-        return newString = str.replace(myRegExp, upperCaser)
+        let myRegExp = new RegExp('(^|\\s)([a-z])', 'ig')
+        let newString = str.replace(myRegExp, upperCaser);
+        return newString;
     } catch(e){
         return 'Please, provide your sentence'
     }
@@ -82,37 +88,37 @@ function isValidAudioFile(str){
     if (str === undefined){
         return false
     } else {
-        let myRegExp = new RegExp ('^[a-z]+(\.mp3|\.flac|\.alac|\.aac)$', 'i')
+        let myRegExp = new RegExp('^[a-z]+(.mp3|.flac|.alac|.aac)$', 'i')
         return myRegExp.test(str);
     }
 }
 function getHexadecimalColors(str){
-    let myRegExp = new RegExp ('#(([a-f0-9]{3,3})|(([a-f0-9]){2,2}([a-f0-9]){2,2}([a-f0-9]){2,2}))(;|\$)', 'gi')
+    let myRegExp = new RegExp('#(([a-f0-9]{3,3})|(([a-f0-9]){2,2}([a-f0-9]){2,2}([a-f0-9]){2,2}))(;|$)', 'gi')
     let finalRegExp = new RegExp('#[a-f0-9]{3,6}','gi')
     let unpreparedArray = str.match(myRegExp)
     if (unpreparedArray === null){
         return [];
     } else{
-            let unfilteredStringAnswer = unpreparedArray.join("");
+            let unfilteredStringAnswer = unpreparedArray.join('');
             let finalArrayAnswer = unfilteredStringAnswer.match(finalRegExp);
             return finalArrayAnswer;
         }
 }
 function isValidPassword(str){
-    let myRegExp = new RegExp ('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[\\w\\W]{8,}$')
+    let myRegExp = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[\\w\\W]{8,}$')
     return myRegExp.test(str);
 }
 function addThousandsSeparators(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 function getAllUrlsFromText(str){
-    let myRegExp = new RegExp ('https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)*\\/', 'gi');
+let myRegExp = new RegExp('https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)*\\/', 'gi');
     try{ 
         if (str === undefined){
             throw new TypeError('error')
         } else if (str.match(myRegExp) === null){
             return []
-        } else  {
+        } else {
             return str.match(myRegExp);
         }
     } catch(e){
